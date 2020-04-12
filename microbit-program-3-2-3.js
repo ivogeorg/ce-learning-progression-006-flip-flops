5434637da0e62a527d81785ff0c242cb4b197fe7
jeremybivans <60162556+jeremybivans@users.noreply.github.com>
2020-04-12 13:02:36 -0600

    Create microbit-program-3-2-3.js

---
+++ microbit-program-3-2-3.js
@@ -0,0 +1,24 @@
+let x: number = 200
+
+input.onButtonPressed(Button.A, function () {
+    x = (x / 2)
+})
+input.onButtonPressed(Button.B, function () {
+    x = (x * 2)
+})
+
+basic.forever(function () {
+
+
+
+    led.toggle(0, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)  // positive edge
+    basic.pause(x)
+
+    led.toggle(0, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)  // negative edge
+    basic.pause(x)
+
+
+})
+
